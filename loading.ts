4717070d6bb1b2320c94b0576f11444e5c64f0dc
godud2604 <ees238@naver.com>
2022-04-28 17:14:50 +0900

    Refactor from if to switch

--- loading.ts
+++ loading.ts
@@ -20,14 +20,21 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  // 대게 유니온 타입경우 여러 케이스별로 다른 로직을 적용할때는 switch가 더 좋다.
+  // 다른 타입(retrying 재시도중)을 추가 한다면 추가된 케이스는 전부다 else에 들어가서 예상하지 못한 오류가 발생할 것.
   function printLoginState(state: ResourceLoadState) {
-    if (state.state === 'loading') {
-      console.log('👀 loading...');
-    } else if (state.state === 'success') {
-      console.log('😃 loaded');
-    } else {
-      console.log('😱 no network');
-    }
+    switch (state.state) {
+      case 'loading':
+        console.log('loading...');
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`);
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }   
   }
 
   printLoginState({ state: 'loading' }); // 👀 loading...
