052ddc0e46d94e89dfcd022a0ed6b0540b8d1e33
godud2604 <ees238@naver.com>
2022-05-17 18:38:18 +0900

    9-1. type alias vs interface

---
+++ 9-advanced/9-1-interface.ts
@@ -0,0 +1,52 @@
+type PositionType = {
+  x: number;
+  y: number;
+}
+
+interface PositionInterface {
+  x: number;
+  y: number;
+}
+
+// object 🔆
+const obj1: PositionType = {
+  x: 1,
+  y: 1,
+};
+const obj2: PositionInterface = {
+  x: 1,
+  y: 1,
+  z: 2,
+};
+
+// class 🔆
+class Pos1 implements PositionType {
+  x: number;
+  y: number;
+}
+class Pos2 implements PositionInterface {
+  x: number;
+  y: number;
+  z: number;
+}
+
+// Extends 🔆
+interface ZPositionInterface extends PositionInterface {
+  z: number;
+}
+type ZPositionType = PositionType & { z: number };
+
+// ❗️ only interfaces can be merged.
+interface PositionInterface {
+  z: number;
+}
+
+// ❗️ Type aliases can use computed properties
+type Person = {
+  name: string,
+  age: number,
+}
+type Name = Person['name']; // string
+
+type NumberType = number;
+type Direction = 'left' | 'right';
